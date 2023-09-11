9938a8cd9e34fd71431e7721b4f1bf7198aa4df1
lucia.pena.perez@scilifelab.se <lucia.penaperez@hasta-login.local>
2023-09-11 11:24:10 +0200

    fixing drop_counts.py

--- bin/drop_counts.py
+++ bin/drop_counts.py
@@ -7,56 +7,57 @@ from pathlib import Path
 
 import pandas as pd
 
-translator = {
+TRANSLATOR = {
     "unstranded": 1,
     "forward": 2,
     "reverse": 3,
 }
 
+STANDARD_CHROMOSOMES = [
+    "chr1",
+    "chr2",
+    "chr3",
+    "chr4",
+    "chr5",
+    "chr6",
+    "chr7",
+    "chr8",
+    "chr9",
+    "chr10",
+    "chr11",
+    "chr12",
+    "chr13",
+    "chr14",
+    "chr15",
+    "chr16",
+    "chr17",
+    "chr18",
+    "chr19",
+    "chr20",
+    "chr21",
+    "chr22",
+    "chrX",
+    "chrY",
+    "chrM",
+]
 
-def get_non_std_genes(gtf: Path) -> set[str]:
+
+def get_non_std_genes(gtf: Path) -> Set[str]:
     """Create list of genes not belonging to chr1-21 or chrM"""
-    std_chrs = [
-        "chr1",
-        "chr2",
-        "chr3",
-        "chr4",
-        "chr5",
-        "chr6",
-        "chr7",
-        "chr8",
-        "chr9",
-        "chr10",
-        "chr11",
-        "chr12",
-        "chr13",
-        "chr14",
-        "chr15",
-        "chr16",
-        "chr17",
-        "chr18",
-        "chr19",
-        "chr20",
-        "chr21",
-        "chr22",
-        "chrX",
-        "chrY",
-        "chrM",
-    ]
     gene_id_regex = re.compile('gene_id "(.+?)"')
     genes_to_exclude = []
     with open(gtf, "r") as gtf_file:
         for line in gtf_file:
             if line.startswith("#"):
                 continue
-            if line.split()[0] in std_chrs:
+            if line.split()[0] in STANDARD_CHROMOSOMES:
                 continue
             gene_id = re.search(gene_id_regex, line)
             genes_to_exclude.append(gene_id.group(1))
     return set(genes_to_exclude)
 
 
-def read_star_gene_cnts(sample: str, star: Path, strandedness: str) -> dict:
+def read_star_gene_counts(sample: str, star: Path, strandedness: str) -> dict:
     """Read gene count file(s) from STAR output to return sample_ids."""
     sample_ids = {}
     gene_ids = {}
@@ -64,15 +65,15 @@ def read_star_gene_cnts(sample: str, star: Path, strandedness: str) -> dict:
         for line in in_tab:
             if not line.startswith("N_"):
                 gene_id = line.split()[0]
-                strand = translator[strandedness]
-                counts = line.split()[strand]
-                gene_ids[gene_id] = int(counts)
+                strand = TRANSLATOR[strandedness]
+                counts = int(line.split()[strand])
+                gene_ids[gene_id] = counts
     gene_ids = OrderedDict(sorted(gene_ids.items()))
     sample_ids[sample] = gene_ids
     return sample_ids
 
 
-def dict_to_counts(gene_ids_dict: dict) -> pd:
+def get_counts_from_dict(gene_ids_dict: dict) -> DataFrame:
     """Transform gene ids dict into count_table"""
     one_sample = next(iter(gene_ids_dict))
     gene_list = list(gene_ids_dict[one_sample].keys())
@@ -86,20 +87,17 @@ def dict_to_counts(gene_ids_dict: dict) -> pd:
     return count_table
 
 
-def transform_to_table(gene_ids_dict: dict, outfile: Path, genes_to_exlude: set[str], ref_count_file: Path):
+def get_tsv_from_dict(gene_ids_dict: dict, outfile: Path, ref_count_file: Path):
     """Transform dictionary into tsv friendly."""
-    count_table = dict_to_counts(gene_ids_dict)
-    final_table = None
+    count_table = get_counts_from_dict(gene_ids_dict)
     if ref_count_file:
-        if ref_count_file.endswith(".gz"):
+        if ref_count_file.suffix == ".gz":
             ref_table = pd.read_csv(ref_count_file, compression="gzip", sep="\t", header=0, index_col=0)
         else:
             ref_table = pd.read_csv(ref_count_file, sep="\t", header=0, index_col=0)
-        final_table = count_table.combine_first(ref_table)
-    else:
-        final_table = count_table
-    final_table.drop(genes_to_exclude, inplace=True)
-    final_table.to_csv(outfile, compression="gzip", sep="\t", header=True)
+        count_table = count_table.combine_first(ref_table)
+    count_table.drop(genes_to_exclude, inplace=True)
+    count_table.to_csv(outfile, compression="gzip", sep="\t", header=True)
 
 
 if __name__ == "__main__":
@@ -123,8 +121,8 @@ if __name__ == "__main__":
     master_dict = {}
     for index, sample_id in enumerate(args.samples):
         master_dict.update(
-            read_star_gene_cnts(sample=sample_id, star=args.star[index], strandedness=args.strandedness[index])
+            read_star_gene_counts(sample=sample_id, star=args.star[index], strandedness=args.strandedness[index])
         )
 
     genes_to_exclude = get_non_std_genes(args.gtf)
-    transform_to_table(master_dict, args.output, genes_to_exclude, args.ref_count_file)
+    get_tsv_from_dict(master_dict, args.output, genes_to_exclude, args.ref_count_file)
