885d09e35a961cfb631ab1f52b8f7682ce0abe81
jemten <jemten@users.noreply.github.com>
2023-10-27 16:21:29 +0200

    changing some references from nf-core to gms

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -1,5 +1,5 @@
 //
-// This file holds several functions specific to the main.nf workflow in the nf-core/tomte pipeline
+// This file holds several functions specific to the main.nf workflow in the genomic-medicine-sweden/tomte pipeline
 //
 
 import nextflow.Nextflow
