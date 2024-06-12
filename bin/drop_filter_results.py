38fa5249e1224e9678825dacab750c1cd42e77b8
Lucpen <lucia.penaperez@hasta-login.local>
2024-06-12 15:20:56 +0200

    unify drop output column names

--- bin/drop_filter_results.py
+++ bin/drop_filter_results.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
-
+from re import split
+from re import match
 import pyreadr
 from pandas import DataFrame, concat, read_csv
 
-SCRIPT_VERSION = "v1.2"
+SCRIPT_VERSION = "v1.3"
 GENE_PANEL_HEADER = ["chromosome", "gene_start", "gene_stop", "hgnc_id", "hgnc_symbol"]
 GENE_PANEL_COLUMNS_TO_KEEP = ["hgnc_symbol", "hgnc_id"]
 
@@ -66,6 +67,7 @@ def filter_by_gene_panel(
             df_family_top_hits, left_on="hgnc_symbol", right_on="hgncSymbol"
         )
         df_clinical = df_clinical.drop(columns=["hgnc_symbol"])
+        df_clinical = camelize_dataframe_columns(df_clinical)
         file_name_clinical = file_name_research.replace("research.tsv", "clinical.tsv")
         df_clinical.to_csv(file_name_clinical, sep="\t", index=False, header=True)
 
@@ -114,8 +116,9 @@ def filter_outrider_results(
     df_family_annotated_aberrant_expression_top_hits = annotate_with_drop_gene_name(
         df_family_aberrant_expression_top_hits, out_drop_gene_name
     )
+    # df_family_annotated_aberrant_expression_top_hits = camelize_dataframe_columns(df_family_annotated_aberrant_expression_top_hits)
     file_name_research = f"{case_id}{output_name_fragment_ae}_research.tsv"
-    df_family_annotated_aberrant_expression_top_hits.to_csv(
+    camelize_dataframe_columns(df_family_annotated_aberrant_expression_top_hits).to_csv(
         file_name_research, sep="\t", index=False, header=True
     )
     filter_by_gene_panel(
@@ -148,12 +151,25 @@ def filter_fraser_result(
         df_results_family_aberrant_splicing, out_drop_gene_name
     )
     file_name_research = f"{case_id}{output_name_fragment_as}_research.tsv"
-    df_results_family_aberrant_splicing.to_csv(
+    # df_results_family_aberrant_splicing = camelize_dataframe_columns(df_results_family_aberrant_splicing)
+    camelize_dataframe_columns(df_results_family_aberrant_splicing).to_csv(
         file_name_research, sep="\t", index=False, header=True
     )
     filter_by_gene_panel(
         df_results_family_aberrant_splicing, gene_panel, file_name_research
     )
+
+
+def camelize_dataframe_columns(df):
+    def camelize(string):
+        if match("^[a-z]+([A-Z][a-z0-9]*)*$", string):
+            return string
+        parts = split("([^a-zA-Z0-9])", string)
+        camelized = "".join(part.capitalize() for part in parts if part.isalnum())
+        return camelized[0].lower() + camelized[1:]
+
+    df.columns = [camelize(col) for col in df.columns]
+    return df
 
 
 def parse_args(argv=None):
